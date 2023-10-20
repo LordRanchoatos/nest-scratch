f5e16fa2e608501eca5f2562415f952742741e9b
Atolagbe David Tolulope <atolagbedt@gmail.com>
2023-10-20 08:01:12 +0100

    advance routing with controller decoratrors

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get} from '@nestjs/common';
 
-@Controller()
+@Controller('app')
 export class AppController{
     @Get()
     getRoute(){
         return 'Hi there!!!';
+    }
+
+    @Get('/info')
+    getInfo(){
+        return 'Contact us on Disord.'
     }
 }
